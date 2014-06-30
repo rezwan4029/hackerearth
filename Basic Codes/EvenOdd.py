561fcb9db55102ef18990c35261269ce9e80d839
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-07-01 01:54:31 +0600

    Some basic Codes

---
+++ Basic Codes/EvenOdd.py
@@ -0,0 +1,6 @@
+x = int( input() );
+
+if( x % 2 == 1 ) :
+    print("Odd");
+else :
+    print("Even");
---
+++ Basic Codes/Recur.py
@@ -0,0 +1,21 @@
+def Sum(x) :
+    if(x == 0) : return 0 ;
+    return x + Sum(x-1);
+
+def Fact(x) :
+    if( x == 0 ) : return 1 ;
+    return x * Fact(x-1);
+
+def Fib(x) :
+    if( x == 0 ) : return 1 ;
+    if( x == 1 ) : return 1 ;
+    return Fib(x-1) + Fib(x-2);
+
+x = int( input() ) ;
+
+
+print( "Sum upto 1.."+ str(x) + " is : "+ str(Sum(x)) );
+
+print( str(x) + " ! = " + str(Fact(x)) ) ;
+
+print( str(x) + "Th Fibonacci = " + str(Fib(x)) ) ;
