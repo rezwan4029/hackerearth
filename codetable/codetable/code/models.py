62bae3570c1a82b9adb330fa1a7e3f763d7df590
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2016-01-08 22:34:13 +0600

    Create Id Generator for url
    Intregate HackerEarth API Parser

--- codetable/codetable/code/models.py
+++ codetable/codetable/code/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+from codetable.lib.utils import id_generator
+from codetable.responselog.models import ResponseLog
+
 
 class Code(models.Model):
 
     DEFAULT_CODE = '''#include <stdio.h>\n\nint main() {\n    printf("Hello World!");\n    return 0;\n}\n'''''
 
-    id = models.CharField(max_length=6, primary_key=True, default=0, editable=False)
+    id = models.CharField(max_length=6, primary_key=True, default=id_generator, editable=False)
     code = models.TextField(default=DEFAULT_CODE)
     input_stdin = models.TextField(null=True)
     run_count = models.IntegerField(default=0)
+    response = models.ForeignKey(ResponseLog, null=True)
     created_at = models.DateTimeField(default=timezone.now())
     updated_at = models.DateTimeField(auto_now=True)
 
