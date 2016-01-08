f2307c6df62a61b8157593146291bed83c91f259
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2016-01-09 00:09:24 +0600

    Refactor view and html

--- codetable/code/views.py
+++ codetable/code/views.py
@@ -16,13 +16,15 @@ from codetable.hackerearth.api_handlers import HackerEarthAPI
 
 class CodeTableView(View):
 
-    """ Create new page for coding """
-    def get(self, request):
+    @staticmethod
+    def get(request):
+        """ Create new page for coding """
         code = Code()
         code.save()
         return HttpResponseRedirect('/code/%s' % code.id)
 
-    def post(self, request, code_id=None):
+    @staticmethod
+    def post(request, code_id=None):
         """ Save code when there is any change """
         try:
             code = get_object_or_404(Code, id=code_id)
@@ -36,24 +38,22 @@ class CodeTableView(View):
 
 
 class CodeTableEditor(View):
-    """
-    For compiling and running code using HACKEREARTH APIs
-    """
 
-    def get(self, request, code_id=None):
+    @staticmethod
+    def get(request, code_id=None):
         template_name = 'html/codetable_editor.html'
         code = get_object_or_404(Code, id=code_id)
         data = {'text': code.code, 'inp': request.POST.get('inp',None)}
         form = CodeForm(data)
         return render(request, template_name, {'code': code.__dict__, 'languages': get_all_supported_languages(), 'form': form})
 
-    def post(self, request, code_id=None):
+    @staticmethod
+    def post(request, code_id=None):
         try:
             get_object_or_404(Code, id=code_id)
             source = request.POST.get('code', '')
             input_data = request.POST.get('input_data', '')
             lang = request.POST.get('lang', None)
-            print('hello : ', input_data)
 
             if input_data:
                 params = RunAPIParameters(
@@ -61,13 +61,17 @@ class CodeTableEditor(View):
                     source=source, lang=lang, id=randint(0, 111111)
                 )
             else:
-                params = RunAPIParameters(client_secret=CLIENT_SECRET, source=source, lang=lang, id=randint(0, 111111))
+                params = RunAPIParameters(
+                    client_secret=CLIENT_SECRET,
+                    source=source,
+                    lang=lang,
+                    id=randint(0, 111111)
+                )
 
             api = HackerEarthAPI(params)
             api.compile()
             ret = api.run()
 
-            # Update the code written by user in the database table
             if ret.status == "CE":
                 ret.output_html = ""
                 ret.memory_used = "0.0"
@@ -88,12 +92,9 @@ class CodeTableEditor(View):
             code.language = lang
             code.input_stdin = input_data
             code.response = response
-
             code.save()
 
             ret.run_count = code.run_count
-            print ret.output_html
-
             ret.last_modified_date = response.created_at.strftime("%b. %d, %Y, %H:%M %p").replace(' 0', ' ')
 
             return HttpResponse(json.dumps(ret.__dict__), content_type="application/json")
